3623c41b0b0c1f516814d79299bbee6084f67b8d
Nikola Milosevic <dzonifu@gmail.com>
2025-01-31 16:50:04 +0100

    nmp

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,14 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // 0.0.0.0
-    port: 5173
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
+    }
+  },
+  server: {
+    hmr: {
+      protocol: 'ws'
     }
   }
 })
